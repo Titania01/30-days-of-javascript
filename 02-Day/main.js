115f88e3c8d04d32348645ba5645404e6eb901dc
Titania01 <adegbitebidmus@gmail.com>
2020-12-28 12:39:49 +0100

    update code base

--- 02-Day/main.js
+++ 02-Day/main.js
@@ -22,3 +22,7 @@ console.log(fullName);
 let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country; // ES5 string addition
 
 console.log(personInfoOne)
+
+const paragraph = "My name is Asabeneh Yetayeh. I live in Finland, Helsinki.\nI am a teacher and I love teaching. I teach HTML, CSS, JavaScript, React, Redux, \nNode.js, Python, Data Analysis and D3.js for anyone who is interested to learn. \nIn the end of 2019, I was thinking to expand my teaching and to reach \nto global audience and I started a Python challenge from November 20 - December 19.\nIt was one of the most rewarding and inspiring experience.\nNow, we are in 2020. I am enjoying preparing the 30DaysOfJavaScript challenge and \nI hope you are enjoying too."
+
+console.log(paragraph)
