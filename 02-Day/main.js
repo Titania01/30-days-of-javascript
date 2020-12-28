108d1f0de0ce0d350596dde3de48b50148dc9dad
Titania01 <adegbitebidmus@gmail.com>
2020-12-28 11:54:40 +0100

    update code base

--- 02-Day/main.js
+++ 02-Day/main.js
@@ -5,3 +5,16 @@ console.log(numBtnZeroAndTen)
 
 let randomNumRoundToFloor = Math.floor(numBtnZeroAndTen)
 console.log(randomNumRoundToFloor)    
+
+let space = ' '           
+let firstName = 'Asabeneh'
+let lastName = 'Yetayeh'
+let country = 'Finland'
+let city = 'Helsinki'
+let language = 'JavaScript'
+let job = 'teacher'
+let quote = "The saying,'Seeing is Believing' is not correct in 2020."
+let quotWithBackTick = `The saying,'Seeing is Believing' is not correct in 2020.`
+
+let fullName = firstName + space + lastName; 
+console.log(fullName);
