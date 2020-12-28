6173bcae0f85d91523eeb46c71b60420c9c10ef8
Titania01 <adegbitebidmus@gmail.com>
2020-12-28 23:51:04 +0100

    update code base

--- 02-Day/main.js
+++ 02-Day/main.js
@@ -1,43 +1,52 @@
-let randomNum = Math.random()
-let numBtnZeroAndTen = randomNum * 11
+// Exercise: Level 1
+let challenge = '30 Days Of JavaScript'
+console.log(challenge)
+console.log(challenge.length)
 
-console.log(numBtnZeroAndTen)
 
-let randomNumRoundToFloor = Math.floor(numBtnZeroAndTen)
-console.log(randomNumRoundToFloor)    
 
-let space = ' '           
-let firstName = 'Asabeneh'
-let lastName = 'Yetayeh'
-let country = 'Finland'
-let city = 'Helsinki'
-let language = 'JavaScript'
-let job = 'teacher'
-let age = 250
-let quote = "The saying,'Seeing is Believing' is not correct in 2020."
-let quotWithBackTick = `The saying,'Seeing is Believing' is not correct in 2020.`
 
-let fullName = firstName + space + lastName; 
-console.log(fullName);
-let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country; // ES5 string addition
+// let randomNum = Math.random()
+// let numBtnZeroAndTen = randomNum * 11
 
-console.log(personInfoOne)
+// console.log(numBtnZeroAndTen)
 
-const paragraph = "My name is Asabeneh Yetayeh. I live in Finland, Helsinki.\nI am a teacher and I love teaching. I teach HTML, CSS, JavaScript, React, Redux, \nNode.js, Python, Data Analysis and D3.js for anyone who is interested to learn. \nIn the end of 2019, I was thinking to expand my teaching and to reach \nto global audience and I started a Python challenge from November 20 - December 19.\nIt was one of the most rewarding and inspiring experience.\nNow, we are in 2020. I am enjoying preparing the 30DaysOfJavaScript challenge and \nI hope you are enjoying too."
+// let randomNumRoundToFloor = Math.floor(numBtnZeroAndTen)
 
-console.log(paragraph)
-console.log('I hope everyone is enjoying the 30 Days Of JavaScript challenge.\nDo you ?') // line break
-console.log('Days\tTopics\tExercises')
-console.log('Day 1\t3\t5')
-console.log('Day 2\t3\t5')
-console.log('Day 3\t3\t5')
-console.log('Day 4\t3\t5')
-console.log('This is a backslash  symbol (\\)') // To write a backslash
-console.log('In every programming language it starts with \"Hello, World!\"')
-console.log("In every programming language it starts with \'Hello, World!\'")
-console.log('The saying \'Seeing is Believing\' isn\'t correct in 2020')
+// console.log(randomNumRoundToFloor)    
 
-let personInfoTwo = `I am ${fullName}. I am ${age}. I live in ${country}.` //ES6 - String interpolation method
-let personInfoThree = `I am ${fullName}. I live in ${city}, ${country}. I am a ${job}. I teach ${language}.`
-console.log(personInfoTwo)
-console.log(personInfoThree)
+// let space = ' '           
+// let firstName = 'Asabeneh'
+// let lastName = 'Yetayeh'
+// let country = 'Finland'
+// let city = 'Helsinki'
+// let language = 'JavaScript'
+// let job = 'teacher'
+// let age = 250
+// let quote = "The saying,'Seeing is Believing' is not correct in 2020."
+// let quotWithBackTick = `The saying,'Seeing is Believing' is not correct in 2020.`
+
+// let fullName = firstName + space + lastName; 
+// console.log(fullName);
+// let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country; // ES5 string addition
+
+// console.log(personInfoOne)
+
+// const paragraph = "My name is Asabeneh Yetayeh. I live in Finland, Helsinki.\nI am a teacher and I love teaching. I teach HTML, CSS, JavaScript, React, Redux, \nNode.js, Python, Data Analysis and D3.js for anyone who is interested to learn. \nIn the end of 2019, I was thinking to expand my teaching and to reach \nto global audience and I started a Python challenge from November 20 - December 19.\nIt was one of the most rewarding and inspiring experience.\nNow, we are in 2020. I am enjoying preparing the 30DaysOfJavaScript challenge and \nI hope you are enjoying too."
+
+// console.log(paragraph)
+// console.log('I hope everyone is enjoying the 30 Days Of JavaScript challenge.\nDo you ?') // line break
+// console.log('Days\tTopics\tExercises')
+// console.log('Day 1\t3\t5')
+// console.log('Day 2\t3\t5')
+// console.log('Day 3\t3\t5')
+// console.log('Day 4\t3\t5')
+// console.log('This is a backslash  symbol (\\)') // To write a backslash
+// console.log('In every programming language it starts with \"Hello, World!\"')
+// console.log("In every programming language it starts with \'Hello, World!\'")
+// console.log('The saying \'Seeing is Believing\' isn\'t correct in 2020')
+
+// let personInfoTwo = `I am ${fullName}. I am ${age}. I live in ${country}.` //ES6 - String interpolation method
+// let personInfoThree = `I am ${fullName}. I live in ${city}, ${country}. I am a ${job}. I teach ${language}.`
+// console.log(personInfoTwo)
+// console.log(personInfoThree)
