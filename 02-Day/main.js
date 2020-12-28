6a9bd0b9dc9a17ff25463b8dba8a036d7062ced8
Titania01 <adegbitebidmus@gmail.com>
2020-12-28 12:12:05 +0100

    update code base

--- 02-Day/main.js
+++ 02-Day/main.js
@@ -13,8 +13,12 @@ let country = 'Finland'
 let city = 'Helsinki'
 let language = 'JavaScript'
 let job = 'teacher'
+let age = 250
 let quote = "The saying,'Seeing is Believing' is not correct in 2020."
 let quotWithBackTick = `The saying,'Seeing is Believing' is not correct in 2020.`
 
 let fullName = firstName + space + lastName; 
 console.log(fullName);
+let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country; // ES5 string addition
+
+console.log(personInfoOne)
