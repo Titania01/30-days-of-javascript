a08a425b6f7a09a0da840cb16c6cb70750b65e80
Titania01 <adegbitebidmus@gmail.com>
2020-12-31 12:08:12 +0100

    first commit

--- 03-Day/main.js
+++ 03-Day/main.js
@@ -32,6 +32,56 @@ console.log(undefined===null)
 console.log(isMarried)
 console.log(2<1)
 
+// 5.Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+console.log(4 > 3)
+console.log(4 >= 3)
+console.log(4 < 3)
+console.log(4 <= 3)
+console.log(4 == 4)
+console.log(4 === 4)
+console.log(4 != 4)
+console.log(4 !== 4)
+console.log(4 != '4')
+console.log(4 == '4')
+console.log(4 === '4')
+console.log('python'.length<'jargon'.length)
+
+// 6. Figure out the result of the following expressions first without using console.log(). After you decide the result confirm it by using console.log()
+
+console.log(4 > 3 && 10 < 12)
+console.log(4 > 3 && 10 > 12)
+console.log(4 > 3 || 10 < 12)
+console.log(4 > 3 || 10 > 12)
+console.log(!(4 > 3))
+console.log(!(4 < 3))
+console.log(!(false))
+console.log(!(4 > 3 && 10 < 12) )
+console.log(!(4 > 3 && 10 > 12))
+console.log(!(4 === '4'))
+//There is no 'on' in both dragon and python
+let isOn = 'python dragon'
+console.log(isOn.search('on'))
+
+// 7. Use the Date object to do the following activities
+
+// i.What is the year today?
+let now = new Date()
+console.log(now.getFullYear())
+// ii.What is the month today as a number?
+console.log(now.getMonth() + 1)
+// iii.What is the date today?
+console.log(now.getDate())
+// iv.What is the day today as a number?
+console.log(now.getDay())
+// v.What is the hours now?
+console.log(now.getHours())
+// vi.What is the minutes now?
+console.log(now.getMinutes())
+// vii.Find out the numbers of seconds elapsed from January 1, 1970 to now.
+console.log(now.getTime())
+
+
+
 
 // const now = new Date()
 // const year = now.getFullYear() // return year
