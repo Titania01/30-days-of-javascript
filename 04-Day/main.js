ef69700dcf7672277566ed0ef39d27826eea9dab
Titania01 <adegbitebidmus@gmail.com>
2021-01-02 11:30:05 +0100

    first commit

--- 04-Day/main.js
+++ 04-Day/main.js
@@ -1,7 +1,13 @@
 // Exercises: Level 1
 // 1.Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he neds to turn 18.
-let user = prompt('Enter your age')
-switch(age){
-  
+let age = prompt('Enter your age')
+let user = age
+switch(user){
+  case age >= 18:
+    console.log('You are old enough to drive')
+    break;
+  case age == 15:
+    console.log('You need 3 more years to be able to drive')
+    break; 
 }
 
