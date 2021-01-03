50dceb6d690da3bad5708d4a8f028db5119b9ae0
Titania01 <adegbitebidmus@gmail.com>
2021-01-03 22:31:46 +0100

    first commit

--- 04-Day/main.js
+++ 04-Day/main.js
@@ -30,3 +30,8 @@ if (a > b) {
 }else {
   console.log('a is less than b')
 }
+
+let isRaining = true
+isRaining
+  ? console.log('You need a rain coat.')
+  : console.log('No need for a rain coat.')
