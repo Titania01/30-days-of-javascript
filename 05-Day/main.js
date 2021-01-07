15fec2640a67fb4b88dceb5e0a1a099d73c7e4f8
Titania01 <adegbitebidmus@gmail.com>
2021-01-07 12:45:22 +0100

    update code base

--- 05-Day/main.js
+++ 05-Day/main.js
@@ -1,16 +1,7 @@
 const fruits = ['banana', 'orange', 'mango', 'lemon']
 let firstFruit = fruits[0] // we are accessing the first item using its index
 
-console.log(firstFruit) // banana
-
-secondFruit = fruits[1]
-console.log(secondFruit) // orange
-
+let lastIndex = fruits.length -1
+console.log(lastIndex)
 let lastFruit = fruits[3]
-console.log(lastFruit) // lemon
-// Last index can be calculated as follows
-
-let lastIndex = fruits.length - 1
-lastFruit = fruits[lastIndex]
-
-console.log(lastFruit)  // lemon
+console.log(lastFruit)
