bdbeae5df377baf9da8453c14bc2732299205524
Titania01 <adegbitebidmus@gmail.com>
2021-01-10 21:27:00 +0100

    update code base

--- 05-Day/main.js
+++ 05-Day/main.js
@@ -1,34 +1,41 @@
-const fruits = ['banana', 'orange', 'mango', 'lemon']
-let firstFruit = fruits[0] // we are accessing the first item using its index
+// Exercise: Level 1
+const arr = []
 
-let lastIndex = fruits.length -1
-console.log(lastIndex)
-let lastFruit = fruits[3]
-console.log(lastFruit)
 
-const arr = Array()
-console.log(arr)
 
-const eightEmptyValues = Array(8).fill(9)
-console.log(eightEmptyValues)
 
-const firstLetter = [1, 2, 3]
-const secondLetter = [4, 5, 6]
-const thirdLetter = firstLetter.concat(secondLetter)
+// const fruits = ['banana', 'orange', 'mango', 'lemon']
+// let firstFruit = fruits[0] // we are accessing the first item using its index
 
-console.log(thirdLetter)
-let index = fruits.indexOf('banana')
+// let lastIndex = fruits.length -1
+// console.log(lastIndex)
+// let lastFruit = fruits[3]
+// console.log(lastFruit)
 
-if (index != -1) {
-  console.log('This fruit does exist in the array')
-}else {
-  console.log('This fruit does not exist in the array')
-}
+// const arr = Array()
+// console.log(arr)
 
-let indexOfAvocado =fruits.indexOf('avocado')
+// const eightEmptyValues = Array(8).fill(9)
+// console.log(eightEmptyValues)
 
-if (indexOfAvocado != -1) {
-  console.log('This fruit does exist in the array')
-}else {
-  console.log('This fruit does not exist in the array')
-}
+// const firstLetter = [1, 2, 3]
+// const secondLetter = [4, 5, 6]
+// const thirdLetter = firstLetter.concat(secondLetter)
+
+// console.log(thirdLetter)
+// let index = fruits.indexOf('banana')
+
+// if (index != -1) {
+//   console.log('This fruit does exist in the array')
+// }else {
+//   console.log('This fruit does not exist in the array')
+// }
+
+// let indexOfAvocado =fruits.indexOf('avocado')
+
+// if (indexOfAvocado != -1) {
+//   console.log('This fruit does exist in the array')
+// }else {
+//   console.log('This fruit does not exist in the array')
+// }
+
