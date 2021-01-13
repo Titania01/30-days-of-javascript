1ff33d96316caf8cc03cfd8a517cb959c79f266f
Titania01 <adegbitebidmus@gmail.com>
2021-01-14 00:22:50 +0100

    update code base

--- 06-Day/main.js
+++ 06-Day/main.js
@@ -9,3 +9,10 @@ for (let i = 5; i >= 0; i--) {
 for (let i = 0; i <= 5; i++) {
   console.log(`${i} * ${i} = ${i * i}`) 
 }
+
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+const newArr = []
+for (let i = 0; i < countries.length; i++) {
+ console.log(newArr.push(countries[i].toUpperCase()))
+}
+
