d51b21b879a07ab26f39b80d5ea4e4476218de56
Titania01 <adegbitebidmus@gmail.com>
2021-03-28 23:54:49 +0100

    feat: update code base

--- 06-Day/main.js
+++ 06-Day/main.js
@@ -30,3 +30,67 @@ for(let i = 0; i <= 10; i++) {
   console.log(i)
 }
 
+for (let i = 0; i <= 10; i++) {
+  console.log(`${i} * ${i} = ${i * i}`)
+}
+
+console.log('i','i^2','i^3')
+for(let i = 0; i <= 10; i++) {
+  console.log(i , i**2,i**3)
+}
+
+for(let i = 0; i <= 100; i++) {
+  if(i%2==0) console.log({even:i})
+  else console.log({odd:i})
+
+}
+for(let i=0; i<=100; i++){
+  if(i===1) {console.log({prime:i}); break;}
+  for(let j=2; j<=i;j++){
+    if(!(i%j)) break;
+  }
+  console.log({prime:i})
+}
+
+// for(let i=0; i<=100; i++) {
+
+// }
+
+
+const add5 = (myNumber) => {
+   return myNumber + 5 
+  }
+  console.log(add5(2))
+
+const randomRgbColor = () => {
+  // const random = Math.floor(Math.random() * 256)
+  const red = Math.floor(Math.random() * 55)
+  const green = Math.floor(Math.random() * 173)
+  const blue = Math.floor(Math.random() * 256)
+
+  return `rgb(${red}, ${green}, ${blue})`
+}
+
+const newCountry = ["ALBANIA", "BOLIVIA", "CANADA", "DENMARK", "ETHIOPIA", "FINLAND", "GERMANY", "HUNGARY", "IRELAND", "JAPAN", "KENYA"]
+
+for (let i = 0; i < newCountry.length; i++) {
+  countries.push(newCountry[i])
+}
+
+console.log(countries);
+
+const newArray = []
+
+for (let i = 0; i < countries.length; i++) {
+  newArray.push(countries[i].length)
+}
+
+console.log(newArray)
+
+let nestedArr = []
+
+for (let i = 0; i < countries.length; i++) {
+  nestedArr[i] = [countries[i], countries[i].slice(0, 3).toUpperCase(), countries[i].length]
+}
+
+console.log(nestedArr)
