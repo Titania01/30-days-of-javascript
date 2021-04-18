f25b9d10bf8241ccc0225c15ee2af04468cd2213
Titania01 <adegbitebidmus@gmail.com>
2021-04-18 23:48:42 +0100

    feat: js object

--- 08-Day/main.js
+++ 08-Day/main.js
@@ -35,6 +35,68 @@ const dog = {
     return 'woof woof'
   }
 }
-console.log(dog)
 const prope = Object.entries(dog)
-console.log(prope)
+dog.breed = 'wolf'
+dog.getDOgInfo = function() {
+  let dogName = this.name
+  let breed = this.breed
+  let age = this.age
+  let view = this.color
+  let statement = `${dogName} is a ${age} yrs old cute dog\nwhich is a ${breed} breed of a golden ${view} color`
+  return statement
+}
+console.log(dog)
+console.log(dog.getDOgInfo())
+
+//Exercises: Level 2
+const users = {
+  Alex: {
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    age: 20,
+    isLoggedIn: false,
+    points: 30
+  },
+  Asab: {
+    email: 'asab@asab.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 25,
+    isLoggedIn: false,
+    points: 50
+  },
+  Brook: {
+    email: 'daniel@daniel.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+    age: 30,
+    isLoggedIn: true,
+    points: 50
+  },
+  Daniel: {
+    email: 'daniel@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  John: {
+    email: 'john@john.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+    age: 20,
+    isLoggedIn: true,
+    points: 50
+  },
+  Thomas: {
+    email: 'thomas@thomas.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  Paul: {
+    email: 'paul@paul.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  }
+}
